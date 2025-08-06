5fefac1048752ba3f7b5697f00d6f8884eebfb93
Timothy0508 <timothytseng508@gmail.com>
2025-08-06 21:37:27 +0800

    feat: Enhance URL management
    add new URL endpoint and response model

--- src/oneurl/manage/manage.py
+++ src/oneurl/manage/manage.py
@@ -1,15 +1,27 @@
 from fastapi import APIRouter
 from sqlmodel import select
 
-from ..models.urls import UrlDB
-from ..models.responses import AllUrlResponse
+from ..models.urls import UrlDB, BaseUrl
+from ..models.responses import AllUrlResponse, AddUrlMess
 from ..services.database import SessionDep
 
 router = APIRouter(prefix="/manage", tags=["Dashboard"])
 
 
 @router.get("/urls")
-async def get_all_urls(session: SessionDep):
+async def get_all_urls(session: SessionDep) -> AllUrlResponse:
     state = select(UrlDB)
     res = session.exec(state).all()
     return AllUrlResponse(urls=list(res))
+
+
+@router.post("/url")
+async def get_new_url(url_record: BaseUrl, session: SessionDep):
+    db_data = UrlDB.model_validate(url_record)
+    session.add(db_data)
+    session.commit()
+    return AddUrlMess(
+        message="Successfully add a new redrict record",
+        alias=url_record.target,
+        location=url_record.location,
+    )
--- src/oneurl/models/responses.py
+++ src/oneurl/models/responses.py
@@ -5,3 +5,12 @@ from .urls import BaseUrl
 
 class AllUrlResponse(BaseModel):
     urls: list[BaseUrl]
+
+
+class BaseMessage(BaseModel):
+    message: str | None
+
+
+class AddUrlMess(BaseMessage):
+    alias: str
+    location: str
