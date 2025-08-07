f6cb517144e6d6f2298637fcd2dbf480abfb2aaa
Timothy0508 <timothytseng508@gmail.com>
2025-08-07 10:40:28 +0800

    feat: Implement redirect functionality with database lookup

--- src/oneurl/url/url.py
+++ src/oneurl/url/url.py
@@ -1,12 +1,19 @@
-from ..models.urls import UrlDB, BaseUrl
-from fastapi import APIRouter, Response
+from ..models.urls import UrlDB
+from ..services.database import SessionDep
+
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import RedirectResponse
+from sqlmodel import select
 
 router = APIRouter(tags=["Redrict Url"], prefix="/rd")
 
 
 @router.get("/{target}")
-def redrict(target: str):
-    return Response(
-        status_code=301,
-        headers=BaseUrl(target=target, location="https://google.com").model_dump(),
-    )
+def redrict(target: str, session: SessionDep) -> RedirectResponse:
+    state = select(UrlDB).where(UrlDB.target == target)
+    res = session.exec(state).first()
+
+    if not res:
+        raise HTTPException(404, "Target not found")
+
+    return RedirectResponse(res.location)
