8d135105ee7c032e91f0172a040d5a6f2ae85b12
Timothy0508 <timothytseng508@gmail.com>
2025-08-12 13:59:23 +0800

    feat: update swagger docs

--- src/oneurl/main.py
+++ src/oneurl/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 from contextlib import asynccontextmanager
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
 
 from .url.url import router as url_router
 from .manage.manage import router as manage_router
@@ -15,10 +17,11 @@ async def init(app: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=init)
+app = FastAPI(lifespan=init, docs_url=None, title="OneUrl")
 
 app.include_router(url_router)
 app.include_router(manage_router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 cors_origins = ["http://localhost", "http://localhost:3000"]
 
@@ -29,3 +32,20 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get("/docs", include_in_schema=False)
+async def swagger_ui_html(req: Request):
+    root_path = req.scope.get("root_path", "").rstrip("/")
+    openapi_url = root_path + app.openapi_url
+    oauth2_redirect_url = app.swagger_ui_oauth2_redirect_url
+    if oauth2_redirect_url:
+        oauth2_redirect_url = root_path + oauth2_redirect_url
+    return get_swagger_ui_html(
+        openapi_url=openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=oauth2_redirect_url,
+        init_oauth=app.swagger_ui_init_oauth,
+        swagger_favicon_url="/static/favicon.ico",
+        swagger_ui_parameters=app.swagger_ui_parameters,
+    )
