97665db547189ab394ee0a884bc9d40c54e7d7e8
Timothy0508 <timothytseng508@gmail.com>
2025-08-13 10:03:53 +0800

    fix: correct response type

--- src/oneurl/url/url.py
+++ src/oneurl/url/url.py
@@ -1,19 +1,19 @@
 from ..models.urls import UrlDB
 from ..services.database import SessionDep
+from ..models.responses import BaseUrl
 
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import RedirectResponse
 from sqlmodel import select
 
 router = APIRouter(tags=["Redrict Url"], prefix="/rd")
 
 
 @router.get("/{target}")
-def redrict(target: str, session: SessionDep) -> RedirectResponse:
+def redrict(target: str, session: SessionDep) -> BaseUrl:
     state = select(UrlDB).where(UrlDB.target == target)
     res = session.exec(state).first()
 
     if not res:
         raise HTTPException(404, "Target not found")
 
-    return RedirectResponse(res.location)
+    return BaseUrl.model_validate(res)
