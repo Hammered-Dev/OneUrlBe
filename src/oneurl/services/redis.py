64f07e401e80c857592998bd242fca4297a4cc6d
Timothy0508 <timothytseng508@gmail.com>
2025-08-13 19:32:06 +0800

    refactor: use .env to set up redis server

--- src/oneurl/services/redis.py
+++ src/oneurl/services/redis.py
@@ -1,8 +1,19 @@
+import os
+
 from redis import Redis
+from dotenv import load_dotenv
+
+load_dotenv()
+
+r = Redis(
+    os.getenv("REDIS_HOST", "localhost"),
+    int(os.getenv("REDIS_PORT", "6379")),
+    int(os.getenv("REDIS_DB", "0")),
+    decode_responses=True,
+)
 
 
 def check_if_default_settings_exists():
-    r = Redis("localhost", decode_responses=True)
     res = r.hexists("settings:general", "delay")
     if not res:
         r.hset("settings:general", mapping={"delay": "3000"})
--- src/oneurl/settings/settings.py
+++ src/oneurl/settings/settings.py
@@ -1,19 +1,17 @@
-from redis import Redis
 from fastapi import APIRouter
 
 from ..models.redis_model import SettingModel
+from ..services.redis import r
 
 router = APIRouter(prefix="/settings", tags=["Settings"])
 
 
 @router.get("")
 async def get_all_settings() -> SettingModel:
-    r = Redis(host="localhost", decode_responses=True)
     res = r.hgetall("settings:general")
     return SettingModel.model_validate(res)
 
 
 @router.put("", status_code=204)
 async def update_all_settings(setting: SettingModel):
-    r = Redis(host="localhost", decode_responses=True)
     r.hset("settings:general", mapping=setting.model_dump())
